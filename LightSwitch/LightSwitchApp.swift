d6c5657528d2ce44d264deace199231e0b810f0c
Marc Schwartz <marcschwartz1990@gmail.com>
2022-09-15 12:33:20 -0400

    Initial Commit

---
+++ LightSwitch/LightSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightSwitchApp.swift
+//  LightSwitch
+//
+//  Created by Marc-Developer on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct LightSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
