4a3870809048a8c24144bcc423ec75e1ca413b0a
Marc Schwartz <marcschwartz1990@gmail.com>
2022-09-15 12:43:18 -0400

    Functional Light Switch

---
+++ LightSwitch/Light.swift
@@ -0,0 +1,34 @@
+//
+//  Light.swift
+//  LightSwitch
+//
+//  Created by Marc-Developer on 9/15/22.
+//
+
+import SwiftUI
+
+//struct Light: View {
+//    @Binding var isOn: Bool
+//    
+//    var body: some View {
+//        ZStack {
+//            ZStack {
+//                if isOn {
+//                    Color.yellow
+//                        .clipShape(Circle())
+//                }
+//            }
+//            .frame(height: 350)
+//            .offset(y: -70)
+//            Image("bulb")
+//        }
+//    }
+//}
+//
+//struct Light_Previews: PreviewProvider {
+//    @State private var lightToggle = false
+//    
+//    static var previews: some View {
+//        Light(isOn: $lightToggle)
+//    }
+//}
---
+++ LightSwitch/PushButton.swift
@@ -0,0 +1,40 @@
+//
+//  PushButton.swift
+//  LightSwitch
+//
+//  Created by Marc-Developer on 9/15/22.
+//
+
+import SwiftUI
+
+//struct PushButton: View {
+//    let titleWhenOn: String
+//    let titleWhenOff: String
+//    @Binding var isOn: Bool
+//    
+//    var onColors = [Color.blue, Color.mint]
+//    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+//    
+//    var body: some View {
+//        Button(isOn ? titleWhenOn : titleWhenOff) {
+//            isOn.toggle()
+//        }
+//        .padding()
+//        .padding(.leading, 50)
+//        .padding(.trailing, 50)
+//        .background(
+//            LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom)
+//        )
+//        .foregroundColor(.white)
+//        .clipShape(Capsule())
+//        .shadow(radius: isOn ? 0 : 5)
+//    }
+//}
+//
+//struct PushButton_Previews: PreviewProvider {
+//    @State private var sample: Bool = false
+//    
+//    static var previews: some View {
+//        PushButton(titleWhenOn: "Turn Off", titleWhenOff: "Turn On", isOn: $sample)
+//    }
+//}
